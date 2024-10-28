17c3160bea11c94ee138018b0e3b204393ed6840
ddamar798 <damarbudiprasetyo714@gmail.com>
2024-10-28 07:30:56 +0700

    fix: edit data

--- oop/property.php
+++ oop/property.php
@@ -10,10 +10,14 @@ class produk{
 $produk1 = new produk();
 $produk1->judul = 'kisah tirex';
 $produk1->penulis = 'damarr';
+$produk1->penerbit = 'PT Jaya mabur';
+$produk1->harga = 50000;
 var_dump($produk1);
 //echo $produk1->judul, $produk1->penulis;
 
 $produk2 = new produk();
 $produk2->judul = 'cara jadi hengker';
 $produk2->penulis = "ddamarr";
+$produk2->penerbit = "PT Ceria Jaya";
+$produk2->harga = 49500;
 var_dump($produk2);
