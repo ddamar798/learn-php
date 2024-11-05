84ae7316bcca6ddddedc82406006eea789020960
ddamar798 <damarbudiprasetyo714@gmail.com>
2024-11-05 19:54:17 +0700

    fix: fix bug

--- oop/staticKeyword.php
+++ oop/staticKeyword.php
@@ -1,13 +1,25 @@
 <?php
 
-class statics{
-    public static $angka = 1;
-    public static function halo(){
-        return "Halo dek1" . self::$angka;
-    }
+// class statics{
+//     public static $angka = 1;
+//     public static function halo(){
+//         return "Halo dek1" . self::$angka;
+//     }
 
+// }
+
+// echo statics::$angka;
+// echo "<br/>";
+// echo statics::halo();
+
+class mobil{
+    public static $warna = "putih";
+
+    public static function ror(){
+        return "Avansa warna " . self::$warna;
+    }
 }
 
-echo statics::$angka;
-echo "<br/>";
-echo statics::halo();
+echo mobil::$warna;
+echo "<hr/>";
+echo mobil::ror();
