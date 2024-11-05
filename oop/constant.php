321491d79bfb2d1ea3c66bb2a4618021abe998b4
ddamar798 <damarbudiprasetyo714@gmail.com>
2024-11-05 20:10:54 +0700

    feat: add file learn constant

---
+++ oop/constant.php
@@ -0,0 +1,14 @@
+<?php
+
+//define bersifat global
+define("nama","Damar");
+define("a","25");
+
+//const bisa disimpan didalam class
+const b = 50;
+
+echo nama;
+echo "<br/>";
+echo "Hasil dari 25 + 50 = " . a + b;
+echo "<hr/>";
+
